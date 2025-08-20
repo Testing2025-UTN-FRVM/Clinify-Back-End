6a68d5fca5f9a5f3763ab48d0557424a9f998a28
Agustin Liendo Ortiz <152560063+AgusLiendo@users.noreply.github.com>
2025-08-20 14:57:02 -0300

    Add TipoUsuario type and extend RegisterDTO with personal information fields

--- src/interfaces/register.dto.ts
+++ src/interfaces/register.dto.ts
@@ -1,6 +1,10 @@
-import {IsNotEmpty, IsString} from "class-validator";
+import {IsDate, IsNotEmpty, IsString} from "class-validator";
+
+export type TipoUsuario = 'user' | 'empleado' | 'paciente';
 
 export class RegisterDTO {
+    tipo: TipoUsuario;
+
     @IsString()
     @IsNotEmpty()
     email: string;
@@ -8,4 +12,23 @@ export class RegisterDTO {
     @IsString()
     @IsNotEmpty()
     password: string;
+
+    //Persona
+    @IsString()
+    nombre: string;
+
+    @IsString()
+    apellido: string;
+
+    @IsDate()
+    fechaNacimiento: Date; // ISO date string
+
+    @IsString()
+    tipoDocumento: string; // e.g., 'DNI', 'Passport'
+
+    @IsString()
+    nroDocumento: string; // e.g., '12345678'
+
+    @IsString()
+    telefono: string; // e.g., '123-456-7890'
 }
