c6be2703ea6a9cdfcfa843781769e6551e925d47
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-08-23 14:43:01 -0300

    Add EmpleadoController and EmpleadoService methods for employee registration and retrieval

--- src/services/empleado/empleado.controller.ts
+++ src/services/empleado/empleado.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Post, Put} from '@nestjs/common';
+import {EmpleadoService} from "./empleado.service";
+import {RegistrarEmpleadoDTO} from "../../interfaces/register.dto";
+import {EmpleadoEntity} from "../../entities/empleado.entity";
 
 @Controller('empleado')
-export class EmpleadoController {}
+export class EmpleadoController {
+    constructor(private readonly empleadoService: EmpleadoService) {}
+
+    @Post()
+    create(@Body() dto: RegistrarEmpleadoDTO): Promise<EmpleadoEntity> {
+        return this.empleadoService.create(dto);
+    }
+
+    @Get('all')
+    findAll(): Promise<EmpleadoEntity[]> {
+        return this.empleadoService.findAll();
+    }
+}
--- src/services/empleado/empleado.service.ts
+++ src/services/empleado/empleado.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {EmpleadoEntity} from "../../entities/empleado.entity";
+import {Repository} from "typeorm";
+import {RegistrarEmpleadoDTO} from "../../interfaces/register.dto";
+import {TipoEmpleadoService} from "../tipo-empleado/tipo-empleado.service";
+import {UsersService} from "../users/users.service";
 
 @Injectable()
-export class EmpleadoService {}
+export class EmpleadoService {
+    constructor(
+        private readonly tipoEmpleadoService: TipoEmpleadoService,
+        private readonly userService: UsersService,
+
+        @InjectRepository(EmpleadoEntity)
+        private readonly empleadoRepository: Repository<EmpleadoEntity>,
+
+
+    ) {}
+
+    async create(dto: RegistrarEmpleadoDTO): Promise<EmpleadoEntity> {
+        const tipo = await this.tipoEmpleadoService.findOne(dto.idTipoEmpleado)
+
+        const user = await this.userService.register(dto.email, dto.password);
+
+        const empleado = this.empleadoRepository.create({
+            nombre: dto.nombre,
+            apellido: dto.apellido,
+            fechaNacimiento: dto.fechaNacimiento,
+            tipoDocumento: dto.tipoDocumento,
+            numeroDocumento: dto.nroDocumento,
+            telefono: dto.telefono,
+            user,
+            tipoEmpleado: tipo
+        });
+
+        return this.empleadoRepository.save(empleado);
+    }
+
+    async findAll(): Promise<EmpleadoEntity[]> {
+        return this.empleadoRepository.find({relations: ["tipoEmpleado", "user"]});
+    }
+}
