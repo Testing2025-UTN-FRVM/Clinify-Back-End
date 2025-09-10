71eef0074248c37f3313e7611187257f405603c4
Agustin Liendo Ortiz <152560063+AgusLiendo@users.noreply.github.com>
2025-09-10 13:33:20 -0300

    Implement CRUD operations in ProcedimientoController

--- src/services/procedimiento/procedimiento.controller.ts
+++ src/services/procedimiento/procedimiento.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
+import {ProcedimientoService} from "./procedimiento.service";
+import {CreateProcedimientoDTO} from "../../interfaces/create/create-procedimiento.dto";
+import {ProcedimientoEntity} from "../../entities/procedimiento.entity";
+import {PatchProcedimientoDTO} from "../../interfaces/patch/patch-procedimiento.dto";
 
 @Controller('procedimiento')
-export class ProcedimientoController {}
+export class ProcedimientoController {
+    constructor(private readonly procedimientoService: ProcedimientoService) {}
+
+    @Post('new')
+    createProcedimiento(@Body() dto:CreateProcedimientoDTO): Promise<ProcedimientoEntity> {
+        return this.procedimientoService.create(dto);
+    }
+
+    @Get('all')
+    findAll(): Promise<ProcedimientoEntity[]> {
+        return this.procedimientoService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id:number): Promise<ProcedimientoEntity> {
+        return this.procedimientoService.findOne(id);
+    }
+
+    @Patch(':id')
+    patch(@Param('id') id:number, @Body() dto:PatchProcedimientoDTO): Promise<ProcedimientoEntity> {
+        return this.procedimientoService.patch(id,dto);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id:number): Promise<{ message: string }> {
+        return this.procedimientoService.delete(id);
+    }
+}
