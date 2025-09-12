2f914a1c4d5552236f5b53e2a792f773fe158002
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-09-12 17:38:07 -0300

    Add CRUD operations for EstadoTurno with authentication and permissions

--- src/services/estado-turno/estado-turno.service.ts
+++ src/services/estado-turno/estado-turno.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import {InjectRepository} from "@nestjs/typeorm";
-import {EstadoTurnoEntity} from "../../entities/estadoTurno.entity";
+import {EstadoTurnoEntity} from "src/entities/estadoTurno.entity";
 import {Repository} from "typeorm";
+import {CreateEstadoTurnoDTO} from "src/interfaces/create/create-estadoTurno.dto";
+import {PatchEstadoTurnoDTO} from "src/interfaces/patch/patch-estadoTurno.dto";
 
 @Injectable()
 export class EstadoTurnoService {
@@ -9,6 +11,23 @@ export class EstadoTurnoService {
         @InjectRepository(EstadoTurnoEntity)
         private readonly estadoTurnoRepository: Repository<EstadoTurnoEntity>,
     ) {}
+
+    async create(dto: CreateEstadoTurnoDTO): Promise<EstadoTurnoEntity> {
+        const estadoTurno = this.estadoTurnoRepository.create(dto);
+        return await this.estadoTurnoRepository.save(estadoTurno);
+    }
+
+    async edit(id:number, dto: PatchEstadoTurnoDTO): Promise<EstadoTurnoEntity> {
+        const estadoTurno = await this.findOne(id);
+        this.estadoTurnoRepository.merge(estadoTurno, dto);
+        return await this.estadoTurnoRepository.save(estadoTurno);
+    }
+
+    async delete(id: number): Promise<{message: string}> {
+        const estadoTurno = await this.findOne(id);
+        await this.estadoTurnoRepository.remove(estadoTurno);
+        return {message: "El estado de turno: "+estadoTurno.nombre+" fue eliminado correctamente"}
+    }
 
     async findOne(id: number): Promise<EstadoTurnoEntity> {
         const estadoTurno = await this.estadoTurnoRepository.findOneBy({id});
