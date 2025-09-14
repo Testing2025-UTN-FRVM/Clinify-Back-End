6601baf153b3e5b50788487e3bae452180199d56
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-09-14 16:01:39 -0300

    Add authentication and permissions to agendarTurno endpoint in TurnoController

--- src/services/turno/turno.controller.ts
+++ src/services/turno/turno.controller.ts
@@ -1,14 +1,16 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {Body, Controller, Post, UseGuards} from '@nestjs/common';
 import {TurnoService} from "./turno.service";
-import {CreateTurnoDTO} from "../../interfaces/create/create-turno.dto";
-import {TurnoEntity} from "../../entities/turno.entity";
+import {CreateTurnoDTO} from "src/interfaces/create/create-turno.dto";
+import {TurnoEntity} from "src/entities/turno.entity";
+import {AuthGuard} from "src/middlewares/auth.middleware";
+import {Permissions} from "src/middlewares/decorators/permissions.decorator";
 
 @Controller('turno')
 export class TurnoController {
-    constructor(private readonly turnoService: TurnoService) {
+    constructor(private readonly turnoService: TurnoService) {}
 
-    }
-
+    @UseGuards(AuthGuard)
+    @Permissions(['TURNOS_CREATE'])
     @Post('agendar')
     agendarTurno(@Body() body: CreateTurnoDTO): Promise<TurnoEntity> {
         return this.turnoService.agendarTurno(body);
