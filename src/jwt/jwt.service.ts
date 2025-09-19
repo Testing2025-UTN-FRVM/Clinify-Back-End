1ef171182cff018c788af5809a5a59bcaa828620
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-09-18 22:29:54 -0300

    Update JWT expiration time from 15 minutes to 1 day

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -8,7 +8,7 @@ export class JwtService {
     config = {
         auth: {
             secret: 'authSecret',
-            expiresIn: '15m',
+            expiresIn: '1d',
         },
         refresh: {
             secret: 'refreshSecret',
