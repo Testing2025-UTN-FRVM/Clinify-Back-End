dee013a790fecbd69ee9b5a499a8443378d2f7b5
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-09-21 23:45:43 -0300

    Enable CORS for localhost and adjust indentation in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,20 @@ import {useContainer} from "class-validator";
 import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {bufferLogs: true});
+    const app = await NestFactory.create(AppModule, {bufferLogs: true});
 
     useContainer(app.select(AppModule), { fallbackOnErrors: true }); // <—
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true, transformOptions: { enableImplicitConversion: true }, }));
 
 
-  await app.listen(process.env.PORT ?? 3001);
+    await app.listen(process.env.PORT ?? 3001);
+
+    app.enableCors({
+        origin: [
+            'http://localhost:4200'
+        ],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        credentials: true
+    });
 }
 bootstrap();
