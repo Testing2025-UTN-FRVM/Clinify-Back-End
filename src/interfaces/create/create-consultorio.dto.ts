5be99bdeb1ba69c8604c5f422e937cf906244772
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-09-29 13:50:39 -0300

    Update validation messages in CreateConsultorioDTO for improved clarity

--- src/interfaces/create/create-consultorio.dto.ts
+++ src/interfaces/create/create-consultorio.dto.ts
@@ -1,11 +1,11 @@
 import {IsNotEmpty, IsNumber, IsString} from "class-validator";
 
 export class CreateConsultorioDTO {
-    @IsNumber()
-    @IsNotEmpty()
+    @IsNumber({},{message: 'El dato introducido debe ser un numero'})
+    @IsNotEmpty({message: 'El numero del consultorio no puede estar vacio'})
     numero: number;
 
-    @IsString()
-    @IsNotEmpty()
+    @IsString({message: 'El dato introducido debe ser un texto'})
+    @IsNotEmpty({message: 'La observacion del consultorio no puede estar vacia'})
     observaciones: string;
 }
