216bf076e1ba7ad2c67bc240dfd56f2bdb3c600d
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-09-29 14:47:02 -0300

    Add unique consultorio validation constraint and integrate into CreateConsultorioDTO

--- src/interfaces/create/create-consultorio.dto.ts
+++ src/interfaces/create/create-consultorio.dto.ts
@@ -1,8 +1,10 @@
 import {IsNotEmpty, IsNumber, IsString} from "class-validator";
+import {IsUniqueConsultorio} from "src/common/validators/unique-consultorio.validator";
 
 export class CreateConsultorioDTO {
     @IsNumber({},{message: 'El dato introducido debe ser un numero'})
     @IsNotEmpty({message: 'El numero del consultorio no puede estar vacio'})
+    @IsUniqueConsultorio()
     numero: number;
 
     @IsString({message: 'El dato introducido debe ser un texto'})
