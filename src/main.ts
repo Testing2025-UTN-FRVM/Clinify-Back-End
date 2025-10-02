cfb373a5bde35b67e3b406598feb5ed52e4d0d52
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-10-02 20:15:47 -0300

    Add endpoint to serve Swagger JSON documentation

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {useContainer} from "class-validator";
 import {ValidationPipe} from "@nestjs/common";
-import reflectMetadata from "reflect-metadata";
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
