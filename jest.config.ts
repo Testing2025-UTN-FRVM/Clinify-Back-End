6c34a573193d93ed2008f860f78a59ef4237921e
Agustin Liendo Ortiz <152560063+AgusLiendo@users.noreply.github.com>
2025-10-15 13:44:40 -0300

    Add Jest configuration file and remove inline Jest settings from package.json

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+module.exports = {
+    preset: 'ts-jest',
+
+    testEnvironment: 'node',
+
+    testMatch: [
+        '<rootDir>/src/**/*.spec.ts',
+        '<rootDir>/tests/**/*.spec.ts',
+    ],
+
+    moduleNameMapper: {
+        '^src/(.*)$': '<rootDir>/src/$1',
+    },
+
+    transformIgnorePatterns: [
+        '/node_modules/(?!uuid)',
+    ],
+
+    transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+    },
+};
